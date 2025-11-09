b6c6c04d3b26d727c1fa3f1984a638151e28ecd3
Shittu Emmanuel <shem4soul@gmail.com>
2025-11-09 17:54:43 +0100

    fix a bug

--- Chapter 6/sandbox.js
+++ Chapter 6/sandbox.js
@@ -73,14 +73,14 @@
 // mssg.setAttribute('style', 'color:red;');
 
 
-const title = document.querySelector('h1');
-// title.setAttribute('style', 'margin: 50px;');
+// const title = document.querySelector('h1');
+// // title.setAttribute('style', 'margin: 50px;');
 
-console.log(title.style);
-console.log(title.style.color);
+// console.log(title.style);
+// console.log(title.style.color);
 
-title.style.margin = '50px';
-title.style.color = 'crimson';
-title.style.fontSize = '60px';
+// title.style.margin = '50px';
+// title.style.color = 'crimson';
+// title.style.fontSize = '60px';
 
-title.style.margin = '';
+// title.style.margin = '';
