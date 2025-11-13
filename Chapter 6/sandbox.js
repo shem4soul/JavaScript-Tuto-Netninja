f81fc1b7686a364e519ed3f46c92111b521560e5
Shittu Emmanuel <shem4soul@gmail.com>
2025-11-13 17:06:29 +0100

    added event listener

--- Chapter 6/sandbox.js
+++ Chapter 6/sandbox.js
@@ -149,6 +149,15 @@
 const ul = document.querySelector('ul');
 // ul.remove();
 
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    //  ul.innerHTML += '<li> something new </li>';
+    const li = document.createElement('li');
+    li.textContent = 'something new to do';
+    ul.append(li);
+})
+
 
 const items = document.querySelectorAll('li');
 
