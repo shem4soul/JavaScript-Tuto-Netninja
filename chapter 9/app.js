fc971f8d23f364579b458dd1cecd42b1392df106
Shittu Emmanuel <shem4soul@gmail.com>
2025-11-26 20:05:22 +0100

    updated premium users

--- chapter 9/app.js
+++ chapter 9/app.js
@@ -1,9 +1,23 @@
 const scores = [10, 30, 15, 25, 50, 40, 5];
 
 
-const filteredScores = scores.filter((score) => {
-    return score > 20;
-});
+// const filteredScores = scores.filter((score) => {
+//     return score > 20;
+// });
 
 
-console.log(filteredScores);
+// console.log(filteredScores);
+
+ const users = [
+    {name: 'Shaun', premium: true},
+    {name: 'Yoshi', premium: false},
+    {name: 'MArio', premium: false},
+    {name: 'chun-li', premium: true}
+ ];
+
+
+ const premiumUsers = users.filter((user) => {
+    return user.premium});
+
+ console.log(premiumUsers);
+ 
