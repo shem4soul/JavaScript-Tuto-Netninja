0a5b889d576026c05b7c9bef68adc004e642a9ea
Shittu Emmanuel <shem4soul@gmail.com>
2025-11-27 08:44:54 +0100

    added reduce method

--- chapter 9/app.js
+++ chapter 9/app.js
@@ -24,28 +24,66 @@
 //map method
 const prices = [20, 10, 30, 25, 15, 40, 80, 5];
 
-const salesPrices = prices.map((price) => {
-     return price /2 } );
+// const salesPrices = prices.map((price) => {
+//      return price /2 } );
 
-console.log(salesPrices);
+// console.log(salesPrices);
 
 
 
-const products = [
-    {name: 'gold star', price: 20}, 
-    {name: 'mushroom', price: 40},
-    {name: 'green shells', price: 30},
-    {name: 'banana skin', price: 10},
-    {name: 'red shells', price: 50}
-];
+// const products = [
+//     {name: 'gold star', price: 20}, 
+//     {name: 'mushroom', price: 40},
+//     {name: 'green shells', price: 30},
+//     {name: 'banana skin', price: 10},
+//     {name: 'red shells', price: 50}
+// ];
 
-const saleProducts = products.map((product) => {
-    if (product.price > 30) {
-         return {name: product.name, price: product.price / 2};
-    }
-    else {
-        return product;
-    }
-});
+// const saleProducts = products.map((product) => {
+//     if (product.price > 30) {
+//          return {name: product.name, price: product.price / 2};
+//     }
+//     else {
+//         return product;
+//     }
+// });
 
-console.log(saleProducts, products);
+// console.log(saleProducts, products);
+
+
+
+// // Reduce method
+// const scores = [10, 20, 60, 40, 70, 90]
+
+// const result = scores.reduce((acc, curr) => {
+//     if (curr > 50){
+//         acc++; 
+//     }
+//     return acc;
+    
+//     }, 0);
+// console.log(result);
+
+
+
+
+// // const scores = [
+// //     {name: 'mario', score: 20},
+// //     {name: 'chun-li', score: 40},
+// //     {name: 'mario', score: 20},
+// //     {name: 'chun-li', score: 40},
+// //     {name: 'mario', score: 20},
+// //     {name: 'chun-li', score: 40},
+// //     {name: 'mario', score: 20},
+// //     {name: 'chun-li', score: 40},
+// //     {name: 'mario', score: 20},
+// //     {name: 'chun-li', score: 40},
+// // ];
+// // const marioScores = scores.reduce((acc, curr) => {
+// //     if(curr.name === 'mario'){
+// //         acc += curr.score; // add only mario's scores
+// //     }
+
+// //     return acc;
+// // }, 0);
+// // console.log(marioScores);
