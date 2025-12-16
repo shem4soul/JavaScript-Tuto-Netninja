98fb1dfc21c00186656e646eac2e42a3a1b50072
Shittu Emmanuel <shem4soul@gmail.com>
2025-12-16 18:19:06 +0100

    refactor API request handling and update endpoint to fetch todos

--- chapter 12/sandbox.js
+++ chapter 12/sandbox.js
@@ -4,8 +4,10 @@ const request = new XMLHttpRequest();
 request.addEventListener('readystatechange', () => {
     //console.log(request, request.readyState);
     if (request.readyState === 4 && request.status === 200) {
-        console.log('Response received:', request.responseText);
+        console.log('Response received:', request,  request.responseText);
+    } else if (request.readyState === 4) {
+        console.log('Could not fetch the data');
     }
 });
-request.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
 request.send();
