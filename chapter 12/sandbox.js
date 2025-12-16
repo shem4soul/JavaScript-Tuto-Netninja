5f03ef75baf33f9c4dc85fd865564e9311ce40cb
Shittu Emmanuel <shem4soul@gmail.com>
2025-12-16 17:30:19 +0100

    added event listener for XMLHttpRequest to handle response

--- chapter 12/sandbox.js
+++ chapter 12/sandbox.js
@@ -1,6 +1,11 @@
 
-
 const request = new XMLHttpRequest();
 
+request.addEventListener('readystatechange', () => {
+    //console.log(request, request.readyState);
+    if (request.readyState === 4 && request.status === 200) {
+        console.log('Response received:', request.responseText);
+    }
+});
 request.open('GET', 'https://jsonplaceholder.typicode.com/posts');
 request.send();
