6b5c2d4deeda35a3d0b25133f7a601f37ebf9d35
Shittu Emmanuel <shem4soul@gmail.com>
2025-12-18 16:46:37 +0100

    add todos.json file and update API request to fetch local todos

--- chapter 12/sandbox.js
+++ chapter 12/sandbox.js
@@ -7,12 +7,13 @@ const getTodos = (callback) =>{
 request.addEventListener('readystatechange', () => {
     //console.log(request, request.readyState);
     if (request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.responseText);
         callback(undefined, request.responseText);
     } else if (request.readyState === 4) {
      callback('Could not fetch data', undefined);
     }
 });
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+request.open('GET', 'todos.json');
 request.send();
 };
 
