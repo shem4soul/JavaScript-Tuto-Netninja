9b04474dc1e6b1ed40d255cb42856e4803fc1d64
Shittu Emmanuel <shem4soul@gmail.com>
2025-12-18 17:33:56 +0100

    refactor getTodos function to return a promise instead of using a callback

--- chapter 12/sandbox.js
+++ chapter 12/sandbox.js
@@ -1,35 +1,33 @@
-const getTodos = (resource, callback) =>{
+const getTodos = (resource) =>{
+
+    return new Promise((resolve, reject) => {
     const request = new XMLHttpRequest();
-
-
 
 
 request.addEventListener('readystatechange', () => {
     //console.log(request, request.readyState);
     if (request.readyState === 4 && request.status === 200) {
         const data = JSON.parse(request.responseText);
-        callback(undefined, request.responseText);
+       resolve(data);
     } else if (request.readyState === 4) {
-     callback('Could not fetch data', undefined);
+        reject('error getting resource');
     }
 });
+
 request.open('GET', resource);
 request.send();
-};
+});
 
+};
 
 console.log(1);
 console.log(2);
 
 
-getTodos('todos/luigi.json', (err, data) => {
-    console.log(data);      
-getTodos('todos/mario.json', (err, data) => {
-    console.log(data);
-    getTodos('todos/shaun.json', (err, data) => {
-        console.log(data);
-    });                         
-});
+getTodos('todos/shaun.json').then(data => {
+    console.log('promise resolved:', data);
+}).catch(err => {
+    console.log('promise rejected:', err);
 });
 
 console.log(3);
