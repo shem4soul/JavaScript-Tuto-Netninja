15e4ea82cadeabc1f3b447dffb1621f1b0a939b7
Shittu Emmanuel <shem4soul@gmail.com>
2025-12-28 18:17:26 +0100

    comment out old getTodos function and related code for clarity

--- chapter 12/sandbox.js
+++ chapter 12/sandbox.js
@@ -1,53 +1,69 @@
-const getTodos = (resource) =>{
+// const getTodos = (resource) => {
+//   return new Promise((resolve, reject) => {
+//     const request = new XMLHttpRequest();
 
-    return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
+//     request.addEventListener("readystatechange", () => {
+//       //console.log(request, request.readyState);
+//       if (request.readyState === 4 && request.status === 200) {
+//         const data = JSON.parse(request.responseText);
+//         resolve(data);
+//       } else if (request.readyState === 4) {
+//         reject("error getting resource");
+//       }
+//     });
 
+//     request.open("GET", resource);
+//     request.send();
+//   });
+// };
 
-request.addEventListener('readystatechange', () => {
-    //console.log(request, request.readyState);
-    if (request.readyState === 4 && request.status === 200) {
-        const data = JSON.parse(request.responseText);
-       resolve(data);
-    } else if (request.readyState === 4) {
-        reject('error getting resource');
-    }
-});
+// console.log(1);
+// console.log(2);
 
-request.open('GET', resource);
-request.send();
-});
+// getTodos("todos/luigi.json")
+//   .then((data) => {
+//     console.log("promise 1 resolved:", data);
+//     return getTodos("todos/mario.json");
+//   })
+//   .then((data) => {
+//     console.log("promise 2 resolved:", data);
+//     return getTodos("todos/shaun.json");
+//   })
+//   .then((data) => {
+//     console.log("promise 3 resolved:", data);
+//   })
+//   .catch((err) => {
+//     console.log("promise rejected:", err);
+//   });
 
+// console.log(3);
+
+// console.log(4);
+
+// //fetch Api
+// fetch("todos/luigi.json")
+//   .then((response) => {
+//     console.log("resolved", response);
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log("rejected", err);
+//   });
+
+//async & await
+const getTodosAsync = async () => {
+  const response = await fetch("todos/luigi.json");
+
+  if (response.status !== 200) {
+    throw new Error("cannot fetch the data");
+  }
+
+  const data = await response.json();
+  return data;
 };
-
-console.log(1);
-console.log(2);
-
-
-getTodos('todos/luigi.json').then(data => {
-    console.log('promise 1 resolved:', data);
-    return getTodos('todos/mario.json');
-}).then(data => {
-    console.log('promise 2 resolved:', data);
-    return getTodos('todos/shaun.json');    
-}).then(data => {
-    console.log('promise 3 resolved:', data);   
-}).catch(err => {
-    console.log('promise rejected:', err);
-});
-
-console.log(3);
-
-console.log(4); 
-
-
-
-//fetch Api
-fetch('todos/luigi.json').then((response) => {
-    console.log('resolved', response);
-    return response.json();
-}).then(data => {
-    console.log(data);
-}).catch((err) => {     
-    console.log('rejected', err);
-}); 
+getTodosAsync()
+  .then((data) => console.log("resolved", data))
+  .catch((err) => console.log("rejected", err));
