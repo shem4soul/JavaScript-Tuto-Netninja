80102829db59e23bc7b8210bf13cb6e8b7514071
Shittu Emmanuel <shem4soul@gmail.com>
2026-01-25 15:54:29 +0100

    getting data from local storage

--- chapter 14/sandbox.js
+++ chapter 14/sandbox.js
@@ -2,3 +2,9 @@
 localStorage.setItem("name", "mario");
 
 localStorage.setItem("age", 55);
+
+//get data from local storage
+let name = localStorage.getItem("name");
+let age = localStorage.getItem("age");
+
+console.log(name, age);
