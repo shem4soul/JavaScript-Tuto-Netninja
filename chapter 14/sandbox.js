e8534afacd2f5a462db17da8a55c123f33e067a9
Shittu Emmanuel <shem4soul@gmail.com>
2026-01-25 16:01:01 +0100

    updating data

--- chapter 14/sandbox.js
+++ chapter 14/sandbox.js
@@ -8,3 +8,11 @@ let name = localStorage.getItem("name");
 let age = localStorage.getItem("age");
 
 console.log(name, age);
+
+//updating data
+localStorage.setItem("name", "luigi");
+localStorage.age = "56";
+
+name = localStorage.getItem("name");
+age = localStorage.getItem("age");
+console.log(name, age);
