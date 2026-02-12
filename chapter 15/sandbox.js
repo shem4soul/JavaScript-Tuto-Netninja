06d3839e4da7986d44bf9298c6772f823381ef58
Shittu Emmanuel <shem4soul@gmail.com>
2026-02-12 19:40:40 +0100

    fix a  bug

--- chapter 15/sandbox.js
+++ chapter 15/sandbox.js
@@ -23,6 +23,10 @@ class User {
 }
 
 class Admin extends User {
+  constructor(username, email, title) {
+    super(username, email);
+    this.title = title;
+  }
   deleteUser(user) {
     users = users.filter((u) => u.username !== user.username);
   }
@@ -30,15 +34,16 @@ class Admin extends User {
 
 const userOne = new User("johnDoe", "johnDoe@gmail.com");
 const userTwo = new User("janeDoe", "janeDoe@fastermail.com");
-const userThree = new Admin("adminUser", "adminuser@gmail.com");
+const userThree = new Admin("adminUser", "adminuser@gmail.com", "black-belt");
 
 let users = [userOne, userTwo, userThree];
 console.log(users);
 
-userThree.deleteUser(userTwo);
-console.log(users);
+// userThree.deleteUser(userTwo);
+// console.log(users);
 
-userOne.deleteUser(userTwo); //error because userOne is not an admin
+console.log(userThree);
+// userOne.deleteUser(userTwo); //error because userOne is not an admin
 
 // console.log(userOne, userTwo);
 
